947126598772ae1c8033b08151df88fe62ab3948
g134 <372668509@qq.com>
2022-08-22 14:07:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -54,5 +54,8 @@ client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
 data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"yutian_days":{"value":get_yutian()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
-res = wm.send_template(user_id, template_id, data)
-print(res)
+
+user_ids=user_id.split(",")
+for userid in user_ids:
+  res = wm.send_template(user_id, template_id, data)
+  print(res)
