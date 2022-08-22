e6cdeb791273eddc21685132b93cc43d2d22839c
g134 <372668509@qq.com>
2022-08-22 22:44:46 +0800

    Update main.py
    增加日期，星期
    更换天气API接口更丰富

--- main.py
+++ main.py
@@ -19,11 +19,23 @@ user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
 
+now_time = today.now()                                               
+week_day = today.isoweekday()                                        
+week = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期天']             
+text_date = '今天是' + str(now_time)[0:10] + ',' + week[week_day - 1]   
+
+
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
   return weather['weather'], math.floor(weather['temp'])
+
+
+def get_weaher2():                                                                                                               
+    url = "https://v0.yiketianqi.com/api?unescape=1&version=v61&appid=43656176&appsecret=I42og6Lm&ext=&cityid=&city=" + city     
+    res = requests.get(url).json()                                                                                               
+    return res['city'], res['wea'], res['tem1'], res['tem2'], res['humidity'], res['air_level'], res['air_tips']                 
 
 def get_yutian():
   delta = today - datetime.strptime(yutianday, "%Y-%m-%d")
@@ -52,8 +64,16 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"yutian_days":{"value":get_yutian()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+# wea, temerature = get_weather()
+# data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"yutian_days":{"value":get_yutian()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+
+res_city, wea, high, low, humidity, air_level, air_tips = get_weaher2() 
+data = {"air_tips": {"value": air_tips}, "air_level": {"value": air_level}, "text_date": {"value": text_date},         
+        "humidity": {"value": humidity, "color": get_random_color()},                                                  
+        "city": {"value": res_city, "color": get_random_color()}, "weathr": {"value": wea},                            
+        "low": {"value": low, "color": get_random_color()}, "high": {"value": high, "color": get_random_color()},      
+        "love_days": {"value": get_count()}, "birthday_left": {"value": get_birthday()},                               
+        "words": {"value": get_words(), "color": get_random_color()}}                                                  
 
 user_ids=user_id.split(",")
 for userid in user_ids:
